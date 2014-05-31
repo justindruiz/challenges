7f2b87b14c569f26e8e932d386751d4b12f42306
justindruiz <justinruizwrestling@gmail.com>
2014-05-31 06:32:22 -0600

    pseudocode.rb

---
+++ pseudocode.rb
@@ -0,0 +1,18 @@
+# Create a new file called pseudocode.rb to work on this exercise.
+# Write out the steps involved in making a tic-tac-toe app
+
+  # Create a board
+  # - use arrays to set up a grid
+  # Create players
+  # - create an x player
+  # - create an o player
+  # Define taking turn
+  # -x marks a space
+  # -o marks a space
+  # What happens after each turn?
+  # -program checks to see if a player has won
+  # -if a player wins the game says you win
+  # -if a player doesn't win the game keeps playing
+  # What happens in the case of a tie?
+  # -tie declared
+  # -new game is played
