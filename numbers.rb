75c0c1dcd9358c6a16f811bc8fc0c6ca96b4f812
justindruiz <justinruizwrestling@gmail.com>
2014-05-31 06:28:30 -0600

    numbers

---
+++ numbers.rb
@@ -0,0 +1,35 @@
+# Create a new file called numbers.rb to work on this exercise.
+puts "Lets do some math:"
+
+puts "25 + 30 / 6 is "
+puts 25 + 30 / 6
+# Calculate and display the answer.
+puts "100 - 25 * 3 / 4 is "
+puts 100 - 25 * 3 / 4
+# Calculate and display the answer
+puts "Is it true that 3 + 2 < 5 - 7?"
+puts (3 + 2) < (5 - 7)
+
+# Determine if the statement is true or false, and display it.
+
+puts "Is 5 greater than -2? "
+ 5 > (- 2)
+
+# Determine if the answer is true or false and display it.
+puts "Is 5 greater or equal to -2? "
+ 5 >= (- 2)
+
+# Determine if the answer is true or false and display it.
+puts "Is 5 less than or equal to -2? "
+ 5 <= (- 2)
+# Determine if the answer is true or false and display it.
+
+number1 = 10
+number2 = 5
+number3 = 2
+
+number1.even?
+number2.odd?
+number3.to_s
+
+# Use at least three methods involving one or all of these numbers
